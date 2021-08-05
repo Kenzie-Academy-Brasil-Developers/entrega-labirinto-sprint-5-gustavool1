f557d7867853d8bf1d75227d077896fa3ca14d1c
gustavo <go8895806@gmail.com>
2021-08-05 20:03:07 -0300

    player Criado

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.onload = criarLabirinto()
+let main = document.getElementsByTagName('main')[0]
 
-console.log(map)
 function criarLabirinto(){
     let main = document.getElementsByTagName('main')[0]
-    for(let i=0; i<map.length;i++){
-        console.log(map[i].length)
+   for(let i=0; i<map.length;i++){
+       
         for(let h=0; h<map[i].length;h++){
             
             if(map[i][h] === 'W'){
@@ -12,13 +12,30 @@ function criarLabirinto(){
                 parede.classList.add('parede')
                 
                 main.appendChild(parede)
-            }if(map[i][h] === ' ' ||map[i][h] === 'S'||map[i][h] === 'F'){
+            }if(map[i][h] === ' '){
                 let buraco = document.createElement('div')
                 buraco.classList.add('buraco')
-               
                 main.appendChild(buraco)
+            }if(map[i][h] === 'S'){
+                let comeco = document.createElement('div')
+                comeco.classList.add('comeco')
+                main.appendChild(comeco)
+            }if(map[i][h] === 'F'){
+                let fim = document.createElement('div')
+                fim.classList.add('fim')
+                main.appendChild(fim)
             }
         }
     }
+    criandoPlayer()
+}
+
+function criandoPlayer(){
+    let comeco = document.getElementsByClassName("comeco")[0]
+    console.log(comeco)
+    let player = document.createElement('div')
+    player.classList.add("player")
+    comeco.appendChild(player)
+        
 }
 
